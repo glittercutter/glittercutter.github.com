976e5cde9d83e4b4a7b74aed4d645d0fa3b1d4d8
Sebastien Raymond <glittercutter@gmail.com>
2011-04-06 10:04:21 -0500

    reverting to static html, github doesnt support php :(

--- header.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-<html>
-<head>
-	<title>glittercutter on github</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-	<link rel="stylesheet" href="mystyle.css">
-</head>
-</html>
